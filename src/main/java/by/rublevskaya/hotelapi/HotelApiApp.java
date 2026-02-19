daedd7e3a7ac407d8788acd6bd0d70f2f15d0904
Kate Rublevskaya <145375460+graffegiraffe@users.noreply.github.com>
2026-02-19 18:34:50 +0100

    chore: initialize project with Java 21, Maven, and core dependencies

---
+++ src/main/java/by/rublevskaya/hotelapi/HotelApiApp.java
@@ -0,0 +1,11 @@
+package by.rublevskaya.hotelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelApiApp.class, args);
+    }
+}
